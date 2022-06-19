f936400e29e9d8479f6ac463ff1e0dc67645a746
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-06-19 12:20:45 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,116 @@
+import typing
+
+from piccolo.utils.pydantic import create_pydantic_model
+from piccolo.engine import engine_finder
+from piccolo.columns import Varchar, Float, Timestamp, Column
+from piccolo.columns.column_types import TimestampNow
+
+from blacksheep import Application, FromJSON, json, Response, Content
+
+from sql.tables import Expense, ExpenseIn
+
+ExpenseModelIn: typing.Any = create_pydantic_model(table=ExpenseIn, model_name=" ExpenseModelIn")
+
+ExpenseModelOut: typing.Any = create_pydantic_model(table=Expense, include_default_columns=True, model_name=" ExpenseModelIn")
+
+ExpenseModelPartial: typing.Any = create_pydantic_model(
+    table=Expense, model_name="ExpenseModelPartial", all_optional=True
+)
+
+
+app = Application()
+
+app.use_cors(
+    allow_methods="GET POST DELETE",
+    allow_origins="https://localhost:4200",
+    allow_headers="Content-Type",
+    max_age=300,
+)
+
+@app.router.get("/expense")
+async def expenses():
+    try:
+        expense = await Expense.select()
+        return expense
+    except:
+        return Response(404, content=Content(b"text/plain", b"Not Found"))
+
+
+@app.router.get("/expense/{id}")
+async def expense(id: int):
+    expense = await Expense.select().where(id==Expense.id)
+    if not expense:
+        return Response(404, content=Content(b"text/plain", b"Id not Found"))
+    return expense
+
+
+@app.router.post("/expense")
+async def create_expense(expense_model: FromJSON[ExpenseModelIn]):
+    try:
+        expense = Expense(**expense_model.value.dict())
+        await expense.save()
+        return ExpenseModelOut(**expense.to_dict())
+    except:
+        return Response(400, content=Content(b"text/plain", b"Bad Request"))
+
+
+@app.router.patch("expense/{id}")
+async def patch_expense(
+        id: int, expense_model: FromJSON[ExpenseModelPartial]
+):
+    expense = await Expense.objects().get(Expense.id == id)
+    if not expense:
+        return Response(404, content=Content(b"text/plain", b"Id not Found"))
+
+    for key, value in expense_model.value.dict().items():
+        if value is not None:
+            setattr(expense, key, value)
+
+    await expense.save()
+    return ExpenseModelOut(**expense.to_dict())
+
+
+@app.router.put("/expense/{id}")
+async def put_expense(
+        id: int, expense_model: FromJSON[ExpenseModelIn]
+):
+    expense = await Expense.objects().get(Expense.id == id)
+    if not expense:
+        return Response(404, content=Content(b"text/plain", b"Id Not Found"))
+
+    for key, value in expense_model.value.dict().items():
+        if value is not None:
+            setattr(expense, key, value)
+
+    await expense.save()
+    return ExpenseModelOut(**expense.to_dict())
+
+
+@app.router.delete("/expense/{id}")
+async def delete_expense(id: int):
+    expense = await Expense.objects().get(Expense.id == id)
+    if not expense:
+        return Response(404, content=Content(b"text/plain", b"Id Not Found"))
+    await expense.remove()
+    return json({"message":"Expense deleted"})
+
+
+
+async def open_database_connection_pool(application):
+    try:
+        engine = engine_finder()
+        await engine.start_connection_pool()
+    except Exception:
+        print("Unable to connect to the database")
+
+
+async def close_database_connection_pool(application):
+    try:
+        engine = engine_finder()
+        await engine.close_connection_pool()
+    except Exception:
+        print("Unable to connect to the database")
+
+
+app.on_start += open_database_connection_pool
+app.on_stop += close_database_connection_pool
