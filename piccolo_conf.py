95debcba7009b40ef45b15f31cebfc90cd909ad7
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-06-19 12:20:30 -0400

    Create piccolo_conf.py

---
+++ piccolo_conf.py
@@ -0,0 +1,16 @@
+from piccolo.conf.apps import AppRegistry
+from piccolo.engine.postgres import PostgresEngine
+
+
+DB = PostgresEngine(config={
+        "database": "expense_tracker",
+        "user": "Your username",
+        "password": "Your password",
+        "host": "localhost",
+        "port": 5432,
+})
+
+
+# A list of paths to piccolo apps
+# e.g. ['blog.piccolo_app']
+APP_REGISTRY = AppRegistry(apps=['sql.piccolo_app'])
